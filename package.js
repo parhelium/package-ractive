9beb857d21d9792401e4c912d5d208c338c29b10
parhelium <me@parhelium.pl>
2014-10-03 10:49:26 +0200

    Updated version of Meteor

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: "Ractive",
-  version: "0.6.0_1",
+  version: "0.6.0_2",
   git: "https://github.com/parhelium/package-ractive"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.1.1');
+  api.versionsFrom('METEOR@0.9.3');
   api.addFiles('lib/ractive-0.6.0.js');
   api.export('Ractive', ['client', 'server']);
 });
