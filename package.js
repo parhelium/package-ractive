0a729a21000b23e305c55ea6c7e61c17496f214a
Kuba Wyrobek <kuba.wyrobek@gmail.com>
2015-05-06 04:30:34 +0200

    update to Ractive@0.7.3

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: "Ractive",
-  version: "0.6.1",
+  version: "0.7.3",
   git: "https://github.com/parhelium/package-ractive"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.3');
-  api.addFiles('lib/ractive-0.6.1.js');
+  api.addFiles('lib/ractive-0.7.3.js');
   api.export('Ractive', ['client', 'server']);
 });
 
